12c7b8478fd15ab191a737bc55e3ee447649683d
Adriana <adriana.capata@gmail.com>
2023-04-27 10:59:47 +0300

    refactor user impersonator logic

--- src/Service/CheckUserImpersonatorService.php
+++ src/Service/CheckUserImpersonatorService.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Evo\SyliusUserImpersonatorPlugin\Service;
 
+use Evo\SyliusUserImpersonatorPlugin\Exception\UserNotFoundException;
 use Sylius\Component\Core\Model\AdminUser;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
@@ -11,34 +12,55 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class CheckUserImpersonatorService
 {
+    protected const SECURITY_ADMIN_TOKEN_NAME = '_security_admin';
+
     public function __construct(private RequestStack $requestStack, private Security $security)
     {
     }
 
-    public function check(): ?UserInterface
+    /**
+     * @psalm-suppress UnusedVariable
+     */
+    public function fetchImpersonatedUser(): UserInterface
+    {
+        $userImpersonator = $this->fetchUsernamePasswordToken();
+
+        if ($userImpersonator?->getUser() === null) {
+            throw new UserNotFoundException('Expected logged in user');
+        }
+
+        /** @psalm-ignore-nullable-return */
+        return $userImpersonator->getUser();
+    }
+
+    public function isImpersonated(): bool
     {
         if ($this->security->getUser() === null) {
-            return null;
+            return false;
         }
+
+        $userImpersonator = $this->fetchUsernamePasswordToken();
+        $userImpersonatorRoles = $userImpersonator?->getRoleNames() ?? [];
+
         //customer user
         $currentUserRoles = $this->security->getUser()->getRoles();
 
-        //admin user that is able to impersonate bc of the ROLE_SWITCH
-        $lastSerializedUsernameToken = $this->requestStack->getSession()->get('_security_admin');
+        return in_array('ROLE_ADMINISTRATION_ACCESS', $userImpersonatorRoles, true) && $currentUserRoles !== $userImpersonatorRoles;
+    }
 
-        //token is null if a customer user logs in
-        if ($lastSerializedUsernameToken === null) {
+
+    public function fetchUsernamePasswordToken(): ?UsernamePasswordToken
+    {
+        /** @psalm-param string $usernamePasswordToken */
+        $usernamePasswordToken = $this->requestStack->getSession()->get(static::SECURITY_ADMIN_TOKEN_NAME);
+
+        if ($usernamePasswordToken === null) {
             return null;
         }
 
         /** @var UsernamePasswordToken $userImpersonator */
-        $userImpersonator = unserialize($lastSerializedUsernameToken, ['allowed_classes' => [UsernamePasswordToken::class, AdminUser::class]]);
-        $userImpersonatorRoles = $userImpersonator->getRoleNames();
+        $userImpersonator = unserialize($usernamePasswordToken, ['allowed_classes' => [UsernamePasswordToken::class, AdminUser::class]]);
 
-        if ($currentUserRoles !== $userImpersonatorRoles && in_array('ROLE_ADMINISTRATION_ACCESS', $userImpersonatorRoles, true)) {
-            return $userImpersonator->getUser();
-        }
-
-        return null;
+        return $userImpersonator;
     }
 }
---
+++ src/Twig/UserImpersonatorHintFilterExtension.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Evo\SyliusUserImpersonatorPlugin\Twig;
+
+use Evo\SyliusUserImpersonatorPlugin\Exception\UserNotFoundException;
+use Evo\SyliusUserImpersonatorPlugin\Service\CheckUserImpersonatorService;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class UserImpersonatorHintFilterExtension extends AbstractExtension
+{
+    public function __construct(private CheckUserImpersonatorService $checkUserImpersonatorService)
+    {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('userImpersonatorHint', [$this, 'returnUserImpersonatorHint']),
+        ];
+    }
+
+    public function returnUserImpersonatorHint(): string
+    {
+        if (!$this->checkUserImpersonatorService->isImpersonated()) {
+            return '';
+        }
+
+        try {
+            return sprintf('Impersonated by %s', $this->checkUserImpersonatorService->fetchImpersonatedUser()->getUserIdentifier());
+        } catch (UserNotFoundException $e) {
+            return '';
+        }
+    }
+}
