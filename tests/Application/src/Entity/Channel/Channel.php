f6ad722ea8db274028a35f6e2c6a48f2e016b1c6
Adriana <adriana.capata@gmail.com>
2023-06-15 15:41:44 +0300

    load entity Channel from the tests/Application

--- tests/Application/src/Entity/Channel/Channel.php
+++ tests/Application/src/Entity/Channel/Channel.php
@@ -2,18 +2,15 @@
 
 declare(strict_types = 1);
 
-namespace Evo\SyliusUserImpersonatorPlugin\Entity\Channel;
+namespace Tests\Adriana\SyliusUserImpersonatorPlugin\Application\Entity\Channel;
 
 use Doctrine\ORM\Mapping as ORM;
+use Evo\SyliusUserImpersonatorPlugin\Entity\Channel\EvoUserImpersonatorChannelTrait;
 use Sylius\Component\Core\Model\Channel as BaseChannel;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_channel")
- */
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_channel')]
-class Channel extends BaseChannel implements EvoUserImpersonatorChannelInterface
+class Channel extends BaseChannel
 {
     use EvoUserImpersonatorChannelTrait;
 }
